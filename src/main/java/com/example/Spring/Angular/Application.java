d715d6efa6bf9a590fbc071b786a4fe591b600c6
Komjáti Ákos <komjati.akos@outlook.com>
2023-09-06 21:59:19 +0200

    User auth started, (login...etc)

--- src/main/java/com/example/Spring/Angular/Application.java
+++ src/main/java/com/example/Spring/Angular/Application.java
@@ -1,13 +1,26 @@
 package com.example.Spring.Angular;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,37 +28,65 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 @Controller
 public class Application {
+    @GetMapping("/user")
+    @ResponseBody
+    public Principal user(Principal user) {
+        return user;
+    }
 
-	@GetMapping("/resource")
-	  @ResponseBody
-	  public Map<String, Object> home() {
-	    Map<String, Object> model = new HashMap<String, Object>();
-	    model.put("id", UUID.randomUUID().toString());
-	    model.put("content", "Hello World");
-	    return model;
-	  }
+    @GetMapping("/resource")
+    @ResponseBody
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
 
-	  @GetMapping(value = "/{path:[^\\.]*}")
-	  public String redirect() {
-	    return "forward:/";
-	  }
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
 
-	  public static void main(String[] args) {
-	    SpringApplication.run(Application.class, args);
-	  }
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Component
-	@ConfigurationProperties("demo")
-	class DemoProperties {
-	  private String value;
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        String encodedPassword = passwordEncoder.encode("password");
 
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
+        UserDetails user = User.withUsername("user")
+            .password(encodedPassword)
+            .roles("USER")
+            .build();
+        
+        return new InMemoryUserDetailsManager(user);
+    }
 }
+
+/*
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfiguration {
+    
+    public void configure(WebSecurity web) throws Exception {
+    	.antMatchers("/index.html", "/", "/home", "/login").permitAll()
+    }
+    
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .httpBasic()
+                .and()
+            .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}*/
